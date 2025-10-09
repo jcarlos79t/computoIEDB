c052cebefd495f2ea1b254921056c00c84901b4e
Juan Carlos Trujillo <jtrujillo@hansa.com.bo>
2025-10-09 14:38:47 -0400

    🐛 [fix] funcional el consumo de api en android y desktop quitando archivos inecesarios

--- composeApp/src/webMain/kotlin/org/jct/iedbs1/network/HttpClientFactory.js.kt
+++ composeApp/src/webMain/kotlin/org/jct/iedbs1/network/HttpClientFactory.js.kt
@@ -2,5 +2,16 @@ package org.jct.iedbs1.network
 
 import io.ktor.client.*
 import io.ktor.client.engine.js.*
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
-actual fun httpClient(): HttpClient = HttpClient(Js)
+actual fun httpClient(): HttpClient = HttpClient(Js){
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true
+        })
+    }
+}
