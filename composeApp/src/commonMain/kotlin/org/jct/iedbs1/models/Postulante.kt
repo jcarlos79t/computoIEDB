b7ae0399c04827e9aef0c1d4fc0f91d1dab98158
Juan Carlos Trujillo <jtrujillo@hansa.com.bo>
2025-11-10 09:13:12 -0400

    :zap: corregido la navegacion

--- composeApp/src/commonMain/kotlin/org/jct/iedbs1/models/Postulante.kt
+++ composeApp/src/commonMain/kotlin/org/jct/iedbs1/models/Postulante.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.Serializable
 data class Postulante(
     var id: String="",
     var nombre: String="",
+    var apellido: String="",
     var color: String="",
     var clase: String="",
     var sexo: String="",
