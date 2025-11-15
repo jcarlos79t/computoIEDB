3025e0e1d1c29ef22f985c707ca44ce9735aaa58
Juan Carlos Trujillo <jtrujillo@hansa.com.bo>
2025-11-14 20:08:05 -0400

    con icono

--- composeApp/src/jvmMain/kotlin/org/jct/iedbs1/main.kt
+++ composeApp/src/jvmMain/kotlin/org/jct/iedbs1/main.kt
@@ -2,11 +2,15 @@ package org.jct.iedbs1
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jetbrains.compose.resources.painterResource
+import votacion_iedbs1.composeapp.generated.resources.Res
+import votacion_iedbs1.composeapp.generated.resources.app_icon
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "votacion_iedbs1",
+        title = "Elecciones IEDB Santiago I",
+        icon = painterResource(Res.drawable.app_icon)
     ) {
         App()
     }
